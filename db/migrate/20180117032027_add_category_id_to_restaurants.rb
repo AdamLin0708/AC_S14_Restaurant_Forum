21259135ab1990863aa9659689bd3ab99869d574
Adamlin0708 <kuso12610@gmail.com>
2018-01-25 01:00:50 +0800

    fix table problem

--- db/migrate/20180117032027_add_category_id_to_restaurants.rb
+++
@@ -1,7 +0,0 @@
-class AddCategoryIdToRestaurants < ActiveRecord::Migration[5.1]
-  def change
-
-    add_column :restaurants, :category_id, :integer
-
-  end
-end
--- db/migrate/20180124164802_create_categories.rb
+++ db/migrate/20180124164802_create_categories.rb
@@ -1,11 +1,13 @@
 class CreateCategories < ActiveRecord::Migration[5.1]
-  def change
+  
+   def change
     create_table :categories do |t|
-
       t.string :name
 
       t.timestamps
-      
     end
+
+    add_column :restaurants, :category_id, :integer
   end
+
 end
