55cb8cab5858104066065c4ec83936525f89ebf6
Adamlin0708 <kuso12610@gmail.com>
2018-01-28 17:21:14 +0800

    setup relation among user, restaurant and favorite

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,7 @@ class Restaurant < ApplicationRecord
   validates_presence_of :name
   belongs_to :category, optional: true
   has_many :comments, dependent: :destroy
+  has_many :favorite, dependent: :destroy
+  has_many :favorited_users, through: :favorite, source: :user
+
 end
